51b3bba6f3ce39eed99d420ab6c8591b10f2a003
renatolmorais <renatolmorais@gmail.com>
2020-06-06 18:36:35 -0300

    consertei a formação da url da promoção

--- main.sh
+++ main.sh
@@ -9,6 +9,7 @@ then
 fi
 
 hardmoburl="https://www.hardmob.com.br/forums/407-Promocoes"
+mainurl="https://www.hardmob.com.br"
 urlfile="urls.txt"
 md5urlfile="md5-$urlfile"
 
@@ -37,7 +38,7 @@ END {
 
 curl -k "$hardmoburl" 2> /dev/null | egrep -i "$regex" | grep -P 'href="(.*?)"' -o | cut -d" " -f1 | cut -d"=" -f2- | tr -d "\"" | sort | uniq | while read url
 do
-	promourl="$hardmoburl/$url"
+	promourl="$mainurl/$url"
 	md5url=`echo $promourl | md5sum -t 2> /dev/null | tr -s " " | cut -d "-" -f1`
 	retval="$(fgrep $md5url $md5urlfile > /dev/null 2>&1 ; echo $?)"
 	if [ ! "$retval" = "0" ]
